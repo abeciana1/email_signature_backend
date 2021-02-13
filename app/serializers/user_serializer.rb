0d256c2b0d7f4d4a7bfb6bcc9d1921903e6977b8
Alex <alex.beciana@gmail.com>
2021-02-13 15:57:49 -0500

    Filled out serializer for user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :first_name, :last_name, :job_title, :department, :company_name, :office_phone, :mobile_phone, :website_url, :email, :address_1, :address_2, :address_3, :address_4, :linkedin, :twitter, :github, :bonus_field
 end
