a6f3e920c68e6712753a76afbc88965166dfa9d0
Alex <alex.beciana@gmail.com>
2021-02-13 15:53:45 -0500

    Created user schema and migrated

--- db/migrate/20210213204548_create_users.rb
+++ db/migrate/20210213204548_create_users.rb
@@ -1,7 +1,23 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.string :job_title
+      t.string :department, :default => ""
+      t.string :company_name, :default => ""
+      t.string :office_phone, :default => ""
+      t.string :mobile_phone, :default => ""
+      t.string :website_url, :default => ""
+      t.string :email
+      t.string :address_1
+      t.string :address_2
+      t.string :address_3, :default => ""
+      t.string :address_4, :default => ""
+      t.string :linkedin, :default => ""
+      t.string :twitter, :default => ""
+      t.string :github, :default => ""
+      t.string :bonus_field, :default => ""
       t.timestamps
     end
   end
